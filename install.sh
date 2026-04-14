aee703348a9eb05219f48cd55a24485309829db3
Keith Valentin <68671699+MrKeith99@users.noreply.github.com>
2026-04-14 10:50:18 +0900

    Add moveit-msgs package to installation script

--- install.sh
+++ install.sh
@@ -5,6 +5,7 @@ echo "╔══╣ Setup: SOBITS INTERFACES (STARTING) ╠══╗"
 # Download ROS packages
 sudo apt update
 sudo apt install -y \
-    ros-$ROS_DISTRO-vision-msgs
+    ros-$ROS_DISTRO-vision-msgs \
+    ros-$ROS_DISTRO-moveit-msgs
 
 echo "╚══╣ Setup: SOBITS INTERFACES (FINISHED) ╠══╝"
